21bd365fbffa884d48545e777bcab479c1f0b019
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-21 10:11:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TropoHangman::Application.config.secret_token = '0ff67fd27d20736eb5d0352f6411393d8aef4ff08ca52811aa9c1f2e4b077a4326c3af67f0f4bca41822b11a3a4a503201e50c719a4089afd5904ef08ee56f53'
