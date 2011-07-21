987c95752ba5bfdd69f1918669f70c4483869212
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-21 10:52:20 -0400

    fixed small typos.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,16 +15,19 @@ class GamesController < ApplicationController
         text = ":new"
       else
         # the game exists
-        if text == ":help" # return helpful info 
+        if text == ":help" 
+          # return helpful info 
           game_message "Available commands are :help, :new, and :word.  Guess a letter by sending a single letter."
         elsif text == ":new" 
           # end the current game and start a new game
-          Game.where(:user => @user, in_progress => true).eadh do |game|
+          Game.where(:user => @user, in_progress => true).each do |game|
             game.update_attributes(:in_progress => false)
           end
           @game = Game.new(:user => @user, :word => Word.random_word, :in_progress => true, :guessed_letters => "")
           game_message = "Tropo Hangman: #{@game.partial_word}; Send a letter to guess or :help for help"
-        elsif text == ":word" # return the word with blanks filled in 
+        elsif text == ":word" 
+          # return the word with blanks filled in 
+          game_message = "Partial word: #{@game.partial_word}; Guessed Letters: #{@game.guessed_letters}"
         else
           guess = text[0]
           @game.guess_letter(guess)
@@ -38,7 +41,6 @@ class GamesController < ApplicationController
       render :json => Tropo::Generator.say(game_message)
     else
       render :json => Tropo::Generator.say("Unsupported operation")
-    end
     end
   end
 
